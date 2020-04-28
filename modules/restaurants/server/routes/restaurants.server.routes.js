d3f0fe30449c5d2bb174aac67713ea1b31dd2658
Christopher Streiffer <ccstreiffer@gmail.com>
2020-04-27 20:26:01 -0500

    Adds order export functionality

--- modules/restaurants/server/routes/restaurants.server.routes.js
+++ modules/restaurants/server/routes/restaurants.server.routes.js
@@ -47,6 +47,11 @@ module.exports = function(app) {
     .put(restaurants.update) // Good
     .delete(restaurants.delete); // Good
 
+  app.route('/api/rest/restaurants/:restaurantId/export')
+    .all(passport.authenticate('jwt', {session: false}))
+    .all(restaurantsPolicy.isAllowed)
+    .post(restaurants.export) // Good
+
   // Finish by binding the restaurant middleware
   app.param('restaurantId', restaurants.restaurantByID);
 
