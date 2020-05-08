4a093cb67558e9ff00c7b10031eb3c0e2ef8612f
Jazz <jpatel@syncro-tech.com>
2020-05-08 11:41:18 -0400

    fixed bug to check for multiple timeslots per restaurant

--- modules/restaurants/server/cron/restaurant.server.cron.js
+++ modules/restaurants/server/cron/restaurant.server.cron.js
@@ -54,7 +54,10 @@ const queryOrders = async (timeslotRange, done) => {
         where: {
           date: {
             [Op.gte]: new Date(Date.now()),
-            [Op.lte]: new Date(Date.now() + TIMESLOT_DAYRANGE * TIMESLOT_HOURRANGE * 60 * 60 * 1000),
+            [Op.lte]: new Date(
+              Date.now() +
+                TIMESLOT_DAYRANGE * TIMESLOT_HOURRANGE * 60 * 60 * 1000
+            ),
           },
           restaurantId: r.id,
         },
@@ -64,6 +67,9 @@ const queryOrders = async (timeslotRange, done) => {
           },
           {
             model: db.restaurant,
+          },
+          {
+            model: db.hospital,
           },
         ],
       });
@@ -75,7 +81,7 @@ const queryOrders = async (timeslotRange, done) => {
         for (const t of timeslots) {
           //timeslots.map( t => {
           //console.log(t)
-          console.log("Timeslot=> ",t.restaurant.name,", ",t.restaurantId,", ",t.user.email,", ",t.user.username,", ",t.date," for hospital: ",t.hospitalId);
+          console.log("Timeslot (",t.date, "/",t.id,")=> ",t.restaurant.name,", ",t.restaurantId,", ",t.user.email,", ",t.user.username,", ",t.date," for hospital: ",t.hospitalId);
 
           console.log("Run " + chalk.blue.bold("order") + " query...");
           let orders = await Order.findAll({
@@ -108,11 +114,14 @@ const queryOrders = async (timeslotRange, done) => {
           });
 
           //Push into map
-          orderMap[t.restaurantId] = {
+          orderMap[t.restaurantId+t.hospitalId] = {
             orders: orders,
             emailRecipient: r.email,
             timeslot: t.date,            
-            restaurantName: r.name,           
+            restaurantName: r.name,   
+            restaurantId: t.restaurantId,
+            hospitalId: t.hospitalId,
+            hospitalName: t.hospital.name
           };
 
           console.log("Orders for restaurant: ", r.name);
@@ -120,38 +129,10 @@ const queryOrders = async (timeslotRange, done) => {
           orders.map((el) => {
             //let el = orders[o];
             //console.log(JSON.stringify(el,null,4))
-            console.log(
-              "[Restaurant/",
-              el.restaurant.id,
-              "],",
-              "[Timeslot/ ",
-              t.id,
-              "],",
-              "[Hospital/ ",
-              el.hospitalId,
-              "],",
-              "[GroupId/",
-              el.groupId,
-              "],",
-              "[OrderId/",
-              el.id,
-              "],",
-              "[MealName/",
-              el.mealName,
-              "], ",
-              "[User/",
-              el.user.username,
-              "], ",
-              "]"
-            );
+            console.log("[Restaurant/",el.restaurant.id,"],","[Timeslot/ ",t.id,"],","[Hospital/ ",el.hospitalId,"],","[GroupId/",el.groupId,"],","[OrderId/",el.id,"],","[MealName/",el.mealName,"], ","[User/",el.user.username,"], ","]");
           });
 
-          console.log(
-            "Done querying orders for restaurant ",
-            t.restaurantId,
-            ", Email: ",
-            t.restaurant.email
-          );
+          console.log("Done querying orders for restaurant ",t.restaurantId,", Email: ",t.restaurant.email);
           console.log("\n\n");
         } //end timeslot mapping
     }
@@ -165,34 +146,39 @@ const queryOrders = async (timeslotRange, done) => {
  * This will flatten all the order details into a CSV string and submit a request to send an email to the 
  * recipient at the restaurant
  * 
- * TODO: Future improvements should include an in-memory stream rather than writing to disc and reading
  * 
  * @param {object} data     JSON object representing the restaurant recipient and all the orders
  */
 var sendMessage = function (data) {
-    
-  console.debug('<sendMessage>'); console.group()
-    
-  // TODO During dev this should be forced to a personal email
-  var receipient = process.env.NODE_ENV === "development" ? "ccstreiffer@gmail.com" : "ccstreiffer@gmail.com"   //data.emailRecipient
-  var restaurantName = data.restaurantName
+  console.debug("<sendMessage>");
+  console.group();
 
-  var orders = data.orders.map(function(order) {
+  
+  //var receipient = process.env.NODE_ENV === "development" ? "ccstreiffer@gmail.com" : "ccstreiffer@gmail.com"   //data.emailRecipient
+  
+  var receipient = process.env.NODE_ENV === "development" ? "jpatel@syncro-tech.com" : "jpatel@syncro-tech.com"; 
+  
+  var restaurantName = data.restaurantName
+  var hospitalName = data.hospitalName
+  var deliveryDate = data.timeslot
+
+  var orders = data.orders.map(function (order) {
     var orderList = [];
     var orderQuantity = Number(order.quantity);
-    for(var i=0; i < orderQuantity; i++) {
+    for (var i = 0; i < orderQuantity; i++) {
       var toPush = order.toJSON();
       toPush.quantity = 1;
       orderList.push(toPush);
     }
-    return orderList
+    return orderList;
   });
 
   // flatten for csv
   var ret = orders.flat(1).map((order) => {
-            
     return {
-      deliveryDate: new Date(order.deliveryDate).toLocaleString("en-US", { timeZone: "America/New_York", }),
+      deliveryDate: new Date(order.deliveryDate).toLocaleString("en-US", {
+        timeZone: "America/New_York",
+      }),
       hospital: order.hospital.name,
       firstName: order.user.firstName,
       lastName: order.user.lastName,
@@ -202,51 +188,70 @@ var sendMessage = function (data) {
       quantity: order.quantity,
       price: order.price,
       total: order.price,
-      orderDate: new Date(order.orderDate).toLocaleString("en-US", { timeZone: "America/New_York", }),
+      orderDate: new Date(order.orderDate).toLocaleString("en-US", {
+        timeZone: "America/New_York",
+      }),
       payStatus: order.payStatus,
       dietaryRestrictions: order.dietaryRestrictions,
       allergies: order.information,
     };
   });
-        
-  new Promise((resolve, reject) => {      
-    resolve(new Buffer.from(parser.parse(ret)));
-  })
-  .then((data) => {
-        
-        // To Do (send Email);
-        var date = new Date().toLocaleString("en-US", { timeZone: "America/New_York" })
-        
-        var mailOptions = {
-          to: [receipient],
-          from: config.mailer.from,
-          subject: "Nourished Order List for " + restaurantName,
-          html:
-            "See attached for complete list of orders for upcoming delivery</br></br>Regards,</br>Team Nourshed",
-          attachments: [
-            {
-              filename:
-                "Order List " +
-                " - " +
-                new Date().toDateString() +
-                " Report.csv",
-              content: data.toString()  //fs.createReadStream(outFile),
-            },
-          ],
-        };
 
-        (async () => {
-            try {
-                await smtpTransport.sendMail(mailOptions);
-                console.log('Email sent')
-            } catch (err) {
-                console.log(err)
-            }
-        })();        
-  })
+  new Promise((resolve, reject) => {
+    let parsedData = "No orders available"
+    try {
+      parsedData = parser.parse(ret);
+    } catch (ex) {
+      console.error('Caught exception while parsing JSON data - ex: ', ex)      
+    }
+    resolve(new Buffer.from(parsedData));
 
-  console.groupEnd(); console.log('</sendMessage>')
-}
+  }).then((data) => {
+    // To Do (send Email);
+    var date = new Date().toLocaleString("en-US", {
+      timeZone: "America/New_York",
+    });
+
+    var mailOptions = {
+      to: [receipient],
+      from: config.mailer.from,
+      subject: "Orders for " + hospitalName + " - " + restaurantName,
+      text:
+        "See attached report for complete list of orders for upcoming delivery at " +
+        deliveryDate.toLocaleString("en-US", {
+          timeZone: "America/New_York",
+        }) +
+        "\n\nRegards,\nTeam Nourshed",
+      attachments: [
+        {
+          filename:
+            "Orders for " +
+            hospitalName +
+            " - " +
+            new Date().toDateString() +
+            " Report.csv",
+          content: data.toString(),
+        },
+      ],
+    };
+
+    (async () => {
+      try {
+        //console.log("sending email...", config.mailer.options)
+        await smtpTransport.sendMail(mailOptions);
+        console.log(new Date() + " - Email sent to: ", receipient);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  });
+
+  console.groupEnd();
+  console.log("</sendMessage>");
+};
+
+
+
 
 const cronDailyUpdate = () => {
   async.waterfall([
